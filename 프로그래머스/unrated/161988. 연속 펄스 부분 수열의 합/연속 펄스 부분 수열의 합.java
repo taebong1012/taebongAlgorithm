1221fb860bc6d92d56a58605842572d9a68aaf30
TaehyunAn <96132804+taebong1012@users.noreply.github.com>
2023-08-01 22:36:10 +0900

    [unrated] Title: 연속 펄스 부분 수열의 합, Time: 72.83 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -4,28 +4,26 @@ import java.lang.*;
 class Solution {
     public long solution(int[] sequence) {
         
-    int length = sequence.length;
+         int sequenceLength = sequence.length;
+        //-1로 시작하는 펄스 수열로 만드는 누적합
+        long[] negative = new long[sequenceLength+1];
+        //1로 시작하는 펄스 수열로 만드는 누적합
+        long[] positive = new long[sequenceLength+1];
+
+        if(sequence.length ==1){
+            return Math.max(sequence[0]*-1, sequence[0]);   
+        }
         
-    //-1로 시작하는 펄스 수열로 만드는 누적합
-    long[] negative = new long[length+1];
+        int pulse = 1;
+        for (int i = 1; i <= sequenceLength; i++) {
+            negative[i] = negative[i - 1] + ((long) sequence[i-1] * pulse);
+            pulse *= -1;
+            positive[i] = positive[i - 1] + ((long) sequence[i-1] * pulse);
+        }
+        //각 배열에서 가장 큰 수와 가장 작은 수를 뽑기위해 오름차순으로 정렬
+        Arrays.sort(negative);
+        Arrays.sort(positive);
         
-    //1로 시작하는 펄스 수열로 만드는 누적합
-    long[] positive = new long[length+1];
-        
-    if (sequence. length ==1) return Math.abs(sequence [0]);
-        
-    int pulse = 1;
-    for (int i = 1; i <= length; i++) {
-        negative[i] = negative[i - 1] + ((long) sequence[i-1] * pulse);
-        pulse *= -1;
-        positive[i] = positive[i - 1] + ((long) sequence[i-1] * pulse);
-    }
-    
-    //각 배열에서 가장 큰 수와 가장 작은 수를 뽑기위해 오름차순으로 정렬
-    Arrays.sort (negative);
-    Arrays.sort(positive);
-    
-    return Math.max(positive[length] - positive[0], negative[length] - negative[0]);
-    
+        return Math.max(positive[sequenceLength] - positive[0], negative[sequenceLength] - negative[0]);
     }
 }
