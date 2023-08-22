efb8481d0b298cb4627e116ed294646e1726c064
TaehyunAn <96132804+taebong1012@users.noreply.github.com>
2023-08-23 02:53:50 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 108 ms, Memory: 12844 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -10,14 +10,10 @@ public class Main {
         for(int i = 0; i < 28; i++) {
             booleans[sc.nextInt()] = true;
         }
-
-        int[] arr = new int[2];
-        int index = 0;
+        
         for(int i = 1; i < 31; i++) {
-            if(!booleans[i]) arr[index++] = i;
+            if(!booleans[i]) System.out.println(i);
         }
 
-        if(arr[0] < arr[1]) System.out.println(arr[0] + "\n" + arr[1]);
-        else System.out.println(arr[1] + "\n" + arr[0]);
     }
 }
