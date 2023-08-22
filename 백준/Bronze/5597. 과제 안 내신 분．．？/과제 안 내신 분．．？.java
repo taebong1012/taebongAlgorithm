0515418ca2f658328e5af3600e7072b74c8cd956
TaehyunAn <96132804+taebong1012@users.noreply.github.com>
2023-08-23 02:54:37 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 72 ms, Memory: 11432 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,16 +1,17 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         boolean[] booleans = new boolean[31];
 
         for(int i = 0; i < 28; i++) {
-            booleans[sc.nextInt()] = true;
+            booleans[Integer.parseInt(br.readLine())] = true;
         }
-        
+
         for(int i = 1; i < 31; i++) {
             if(!booleans[i]) System.out.println(i);
         }
